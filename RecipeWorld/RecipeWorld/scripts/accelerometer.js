09504779b57263a6387bb36b617cf2909a867184
Ivan Tochev <annihilatora@gmail.com>
2014-09-28 23:37:03 +0300

    Accelerometer function added

---
+++ RecipeWorld/RecipeWorld/scripts/accelerometer.js
@@ -0,0 +1,32 @@
+/// <reference path="../kendo/js/jquery.min.js" />
+(function () {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    //device APIs are available
+
+    function onDeviceReady() {
+        navigator.accelerometer.watchAcceleration(onSuccess, onError, { frequency: 100 });
+        $("#s").hide()
+    }
+
+    var watchId = navigator.accelerometer.watchAcceleration(onSuccess, onError, { frequency: 100 });
+    // onSuccess: Get a snapshot of the current acceleration
+
+    function onSuccess(acceleration) {
+        var t = $("#t");
+        //t.html(acceleration.x + " " + acceleration.y + " " + acceleration.z);
+        var currentAcc = 1
+        if (acceleration.x < -10 || acceleration.x > 10) {
+            t.hide();
+            $("#s").show();
+
+            navigator.accelerometer.clearWatch(watchId);
+        }
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        $("#t").html("error")
+    }
+
+}())
