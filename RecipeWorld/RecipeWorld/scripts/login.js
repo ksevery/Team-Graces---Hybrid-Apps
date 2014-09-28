90098d511afc8ab472c2abd881e6ac2108806b62
ksevery <konstantinsevery@gmail.com>
2014-09-28 19:07:02 +0300

    Registration now working

--- RecipeWorld/RecipeWorld/scripts/login.js
+++ RecipeWorld/RecipeWorld/scripts/login.js
@@ -2,17 +2,12 @@ var app = app || {};
 
 app.Login = (function () {
     'use strict';
-    var isInMistSimulator = (location.host.indexOf('icenium.com') > -1);
+    //var isInMistSimulator = (location.host.indexOf('icenium.com') > -1);
 
     var $loginUsername;
     var $loginPassword;
     $loginUsername = $('#loginUsername');
     $loginPassword = $('#loginPassword');
-
-    var show = function () {
-        $loginUsername.val('');
-        $loginPassword.val('');
-    };
 
     var loginViewModel = kendo.observable({
             // Authenticate to use Backend Services as a particular user
@@ -25,18 +20,12 @@ app.Login = (function () {
             app.everlive.Users.login(username, password)
             .then(function () {
                 window.token = app.everlive.token;
+
                 return app.Recipes.load();
             })
             .then(function () {
 
                 app.mobileApp.navigate('views/recipes-view.html');
-
-                //var templateContent = $('recipesTemplate').html();
-                //var template = kendo.template(templateContent);
-                ////console.log(app.Recipes.recipeModel.recipes());
-
-                //var result = kendo.render(template, app.Recipes.recipesModel.recipes());
-                //$('#recipes').html(result);
             })
             .then(null,
                   function (err) {
