b5c1c04abac13bb4f7dc6cc0eac679644ada7670
Ivan Tochev <annihilatora@gmail.com>
2014-09-29 21:32:05 +0300

    cameraModel ... need fix

--- RecipeWorld/RecipeWorld/scripts/camera.js
+++ RecipeWorld/RecipeWorld/scripts/camera.js
@@ -1,32 +1,38 @@
-(function () {
+var app = app || {}
+
+app.Camera = (function () {
     var everlive = new Everlive("UaQVFmYbtMPgqUG0");
 
-  
-    window.listView = kendo.observable({
-        addImage: function () {
+    var cameraModel = function () {
+       window.listView = kendo.observable({
+            addImage: function () {
 
-            var success = function (data) {
-                everlive.Files.create({
-                    Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
-                    ContentType: "image/jpeg",
-                    base64: data
-                })
-            };
 
-            var error = function () {
-                navigator.notification.alert("Unfortunately we could not add the image");
-            };
+                var success = function (data) {
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data
+                    })
+                };
 
-            var config = {
-                destinationType: Camera.DestinationType.DATA_URL,
-                targetHeight: 100,
-                targetWidth: 100,
-                encodingType: Camera.EncodingType.JPEG
+                var error = function () {
+                    navigator.notification.alert("Unfortunately we could not add the image");
+                };
 
-            };
+                var config = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 100,
+                    targetWidth: 100,
+                    encodingType: Camera.EncodingType.JPEG
 
-            navigator.camera.getPicture(success, error, config);
-        }
-    });
-    kendo.bind(? ?);
+                };
+
+                navigator.camera.getPicture(success, error, config);
+            }
+        });
+    }  
+
+    kendo.bind($("#add-recipe"), cameraModel);
+    return cameraModel;
 }())
